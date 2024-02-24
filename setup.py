d798fb75ecd84b021f5e11354e87b484fad5a5b1
Kushal rana <kushalrana448@gmail.com>
2024-02-24 14:19:19 +0530

    workflow

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
@@ -12,18 +12,17 @@ SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "kushalrana448@gmail.com"
 
 
-setuptools.setup(
+setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package for CNN app",
     long_description=long_description,
-    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=find_packages(where="src")
 )
